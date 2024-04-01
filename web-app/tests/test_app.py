5958c895c86fc722571dae531b3861e4d4644813
gboeker <glenda.boeker@gmail.com>
2024-04-01 19:10:32 -0400

    add test files

---
+++ web-app/tests/test_app.py
@@ -0,0 +1,34 @@
+import pytest
+
+from app import *
+
+class Tests:
+    #
+    # Fixtures - these are functions that can do any optional setup or teardown before or after a test function is run.
+    #
+
+    @pytest.fixture
+    def example_fixture(self):
+        """
+        An example of a pytest fixture - a function that can be used for setup and teardown before and after test functions are run.
+        """
+
+        # place any setup you want to do before any test function that uses this fixture is run
+
+        yield  # at th=e yield point, the test function will run and do its business
+
+        # place with any teardown you want to do after any test function that uses this fixture has completed
+
+    #
+    # Test functions
+    #
+
+    def test_sanity_check(self, example_fixture):
+        """
+        Test debugging... making sure that we can run a simple test that always passes.
+        Note the use of the example_fixture in the parameter list - any setup and teardown in that fixture will be run before and after this test function executes
+        From the main project directory, run the `python3 -m pytest` command to run all tests.
+        """
+        expected = True  # the value we expect to be present
+        actual = True  # the value we see in reality
+        assert actual == expected, "Expected True to be equal to True!"
