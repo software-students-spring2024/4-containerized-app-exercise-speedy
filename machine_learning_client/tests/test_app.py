dd98a5dd5898699938bed125da57c2c38707c823
gboeker <glenda.boeker@gmail.com>
2024-04-04 13:42:27 -0400

    use black formatter

--- machine_learning_client/tests/test_app.py
+++ machine_learning_client/tests/test_app.py
@@ -1,26 +1,29 @@
-'''
+"""
 file to test app
-'''
+"""
+
 # import pytest
 
 from machine_learning_client.app import *
 
+
 class Tests:
-    '''
+    """
     test app
-    '''
+    """
+
     def test_sanity_check(self):
-        '''
+        """
         Sanity check 1
-        '''
+        """
         expected = True
         actual = True
         assert actual == expected, "Expected True to be equal to True!"
 
     def test_sanity_check2(self):
-        '''
+        """
         Sanity check 1
-        '''
+        """
         expected = True
         actual = True
         assert actual == expected, "Expected True to be equal to True!"
