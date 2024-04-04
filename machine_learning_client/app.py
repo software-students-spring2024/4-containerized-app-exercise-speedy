75d63c1e77482ed6e550d2850e4e02f0c98830e3
gboeker <glenda.boeker@gmail.com>
2024-04-04 14:37:08 -0400

    basic setup for flask web app

--- machine_learning_client/app.py
+++ machine_learning_client/app.py
@@ -1,3 +1,7 @@
-"""testing docker"""
-
-print("hello")
+"""
+This is a Python application that runs the machine learning
+model for hand gesture recognition. When the web app sends a
+picture of a hand gesture, this client processes the image,
+passes it through the trained model, and determines the
+corresponding emoji representation of the gesture.
+"""
--- web_app/app.py
+++ web_app/app.py
@@ -1,3 +1,26 @@
-"""testing docker"""
+"""
+This is a Flask web application that provides the user interface. 
+Users can access the web app through their browser, use their
+device's camera to take pictures of hand gestures, and see the
+corresponding emoji detected by the machine learning model.
+"""
 
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def display():
+    return "display images here"
+
+
+@app.route("/", methods=["GET", "POST"])
+def image_capture():
+    return "take picture here"
+
+
+# run the app
+if __name__ == "__main__":
+    FLASK_PORT = os.getenv("FLASK_PORT", "5000")
+    app.run(port=FLASK_PORT)
