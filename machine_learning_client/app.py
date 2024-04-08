28a13457090a5d8dd4d1ea0a200519b58c588cf6
shriyakalakata <shriyakalakata123@gmail.com>
2024-04-08 17:34:36 -0400

    Set up the basic hand gesture detector

--- machine_learning_client/app.py
+++ machine_learning_client/app.py
@@ -8,13 +8,57 @@ corresponding emoji representation of the gesture.
 
 from flask import Flask
 import cv2
-import mediapipe
+import mediapipe as mp
 
 app = Flask(__name__)
 
 # add functions for machine leanring client
 
-test_image = cv2.imread("path_to_image.jpg")
+class HandGestureDetector:
+    def __init__(self, static_image_mode = False, max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.5):
+        self.static_image_mode =static_image_mode
+        self.max_num_hands = max_num_hands
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+
+        self.mp_hands = mp.solutions.hands
+        self.hands= self.mp_hands.Hands(self.static_image_mode,
+                                        self.max_num_hands,
+                                        self.min_detection_confidence,    
+                                        self.min_tracking_confidence)
+        
+    def gesture_recognition(self, image):
+        image = cv2.imread('images/test_image1.png')
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        # Process image using MediaPipe hands module
+        results = self.hands.process(image_rgb)
+        
+        # If hand(s) detected
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                pass
+        
+        # If no hand or gesture detected
+        return 0
+    
+    def determine_emoji(self, hand_state):
+        if hand_state == 0:
+            return "No emoji detected"
+        elif hand_state == 1:
+            return "Thumbs up! 👍"
+        elif hand_state == 2:
+            return "Thumbs down! 👎"
+        elif hand_state == 3:
+            return "Thumbs up! 👍"
+        elif hand_state == 4:
+            return "Thumbs up! 👍"
+        elif hand_state == 5:
+            return "Right on! ✊" # maybe change the wording here
+        elif hand_state == 6:
+            return "Hello! 👋"
+        elif hand_state == 7:
+            return "I love you! 👍"
 
 # run the app
 if __name__ == "__main__":
