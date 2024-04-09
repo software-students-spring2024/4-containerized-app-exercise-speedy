134233abdbbadf4648d427c1b071c9b90e38617d
iltenahmet <iltenahmet@gmail.com>
2024-04-09 14:29:38 -0400

    add tests for the web app, add coverage tool as a dependency

--- web_app/tests/test_app.py
+++ web_app/tests/test_app.py
@@ -1,25 +1,28 @@
-"""
-file to test app
-"""
-
 # import pytest
-
-# from web_app.app import display, image_capture
-
+from app import * 
 
 class Tests:
-    """
-    test app
-    """
+    def test_sanity_check(self):
+        expected = True
+        actual = True
+        assert actual == expected, "Expected True to be equal to True!"
+
+    def test_index(self):
+        response = app.test_client().get('/')
+        assert response.status_code == 200
 
     def test_display(self):
-        """
-        test display route
-        """
-        assert True, "Expected True!"
+        response = app.test_client().get('/display')
+        assert response.status_code == 200
 
-    def test_sanity_check2(self):
-        """
-        test image capture route
-        """
-        assert True, "Expected True!"
+    def test_upload_image(self):
+        response = app.test_client().post('/upload_image', data={
+            "image_data": "test"
+        })
+        # test if the response code starts with 3 (3xx codes are for redirection)
+        first_digit = response.status_code // 100
+        assert first_digit == 3
+
+
+
+
