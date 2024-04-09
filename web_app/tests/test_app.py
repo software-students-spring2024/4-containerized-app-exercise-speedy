521141f1cce180b8312ff6a7efebef31927a8701
iltenahmet <iltenahmet@gmail.com>
2024-04-09 15:49:42 -0400

    reformat test_app.py with black

--- web_app/tests/test_app.py
+++ web_app/tests/test_app.py
@@ -1,31 +1,30 @@
-''' Module for testing app.py. '''
+""" Module for testing app.py. """
 
 from web_app.app import app
 
+
 class Tests:
-    ''' Class for testing app.py '''
+    """Class for testing app.py"""
 
     def test_sanity_check(self):
-        ''' Function sanity check. '''
+        """Function sanity check."""
         expected = True
         actual = True
         assert actual == expected, "Expected True to be equal to True!"
 
     def test_index(self):
-        ''' Function testing index.'''
-        response = app.test_client().get('/')
+        """Function testing index."""
+        response = app.test_client().get("/")
         assert response.status_code == 200
 
     def test_display(self):
-        ''' Function testing display.'''
-        response = app.test_client().get('/display')
+        """Function testing display."""
+        response = app.test_client().get("/display")
         assert response.status_code == 200
 
     def test_upload_image(self):
-        ''' Function testing upload image.'''
-        response = app.test_client().post('/upload_image', data={
-            "image_data": "test"
-        })
+        """Function testing upload image."""
+        response = app.test_client().post("/upload_image", data={"image_data": "test"})
         # test if the response code starts with 3 (3xx codes are for redirection)
         first_digit = response.status_code // 100
         assert first_digit == 3
